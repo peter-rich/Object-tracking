aa79460fe2a7652549b185b667691889055fc785
peter-rich <yangzhanfu111@gmail.com>
2016-07-14 17:37:39 +0800

    1

---
+++ object tracking app/Tracking/AppDelegate.h
@@ -0,0 +1,16 @@
+//
+//  AppDelegate.h
+//
+//  Created by FloodSurge on 18/5/16.
+//  Copyright (c) 2016年 Zhanfu Yang All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ object tracking app/Tracking/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  VisionSeg
+//
+//  Created by FloodSurge on 18/5/16.
+//  Copyright (c) 2016年 Zhanfu Yang All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
